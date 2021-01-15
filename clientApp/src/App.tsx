34dfadaff9a25da743f6bc0e464f9aec8325aec1
dmitry <dimaxik97@gmail.com>
2021-01-15 07:38:49 +0300

    first commit

---
+++ clientApp/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css'
+
+import "normalize.css";
+
+const App:React.FC=()=> {
+  return (
+    <h1 className={styles.title}>Hello world</h1>
+  );
+}
+
+export default App;
---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from "express";
+const config = require("config");
+const path = require("path");
+
+const app = express();
+
+// initialize server app
+app.use(express.static(path.join(__dirname, "../clientApp/dist")));
+
+// make server listen on some port
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = config.get("port") || 5000;
+
+function start() {
+  try {
+    app.listen(PORT, () =>
+      console.log(`App has been started on port ${PORT}...`)
+    );
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
